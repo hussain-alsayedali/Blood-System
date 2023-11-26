ff13e4dd280efb382bcc97cfc63a9b2d4f9c9f97
Feras532 <saf_523_@hotmail.com>
2023-11-26 22:18:16 +0300

    lil update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,3 @@
-// src/redux/store.js
 import { createStore } from "redux";
 
 const initialState = {
@@ -7,13 +6,16 @@ const initialState = {
 
 function userReducer(state = initialState, action) {
   switch (action.type) {
+    // If the action type is "SET_USER", update the user property in the state
     case "SET_USER":
+      // Return a new state object with the user property updated with the action payload
       return { ...state, user: action.payload };
+    // Default case: return the current state if the action type is not recognized
     default:
       return state;
   }
 }
-
 const store = createStore(userReducer);
 
+// Export the created store to be used in the application
 export default store;
