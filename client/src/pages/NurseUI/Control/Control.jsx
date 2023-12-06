e65952f3982c472697059baac40a586f7391fe72
Feras532 <saf_523_@hotmail.com>
2023-12-06 08:52:46 +0300

    client side edit

--- client/src/pages/NurseUI/Control/Control.jsx
+++ client/src/pages/NurseUI/Control/Control.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Control.css";
 import Axios from "axios";
-import { Link } from "react-router-dom";
 
 function Control() {
   const [search, setSearch] = useState("");
-  const [users, setUsers] = useState([
-    // Add more people baba
-  ]);
+  const [users, setUsers] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [formData, setFormData] = useState({});
 
   useEffect(() => {
     Axios({
@@ -21,6 +20,7 @@ function Control() {
       })
       .catch((error) => console.error(error));
   }, []);
+
   const filterdUsers = users.filter((user) =>
     user.id.toString().includes(search)
   );
@@ -29,15 +29,25 @@ function Control() {
     setSearch(event.target.value);
   };
 
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const startEditing = (user) => {
+    setEditingId(user.id);
+    setFormData(user);
+  };
+
+  const saveChanges = (user) => {
+    // TODO: save the changes to the backend
+    setEditingId(null);
+  };
+
   const deleteUser = (userId, userType) => {
     Axios({
       method: "DELETE",
       url: `http://localhost:2121/nurse/deleteUser?id=${userId}&type=${userType}`,
       withCredentials: true,
-      data: {
-        _method: "DELETE",
-        // Other request data...
-      },
     });
   };
 
@@ -72,30 +82,83 @@ function Control() {
             <tr key={user.id + user.type}>
               <td>{user.id}</td>
               <td>{user.type}</td>
-              <td>
-                {user.firstName} {user.lastName}
+              <td>{editingId === user.id ? (
+                <input
+                  name="name"
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                `${user.firstName} ${user.lastName}`
+              )}
               </td>
-              <td>
-                {new Date().getFullYear() - new Date(user.birth).getFullYear()}
+              <td>{editingId === user.id ? (
+                <input
+                  name="birth"
+                  value={new Date(formData.birth).getFullYear()}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                new Date().getFullYear() - new Date(user.birth).getFullYear()
+              )}
               </td>
-              <td>{user.bloodType}</td>
-              <td>{user.email}</td>
-              <td>{user.weight}</td>
-              <td>{user.phone}</td>
+              <td>{editingId === user.id ? (
+                <input
+                  name="bloodType"
+                  value={formData.bloodType}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                user.bloodType
+              )}
+              </td>
+              <td>{editingId === user.id ? (
+                <input
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                user.email
+              )}
+              </td>
+              <td>{editingId === user.id ? (
+                <input
+                  name="weight"
+                  value={formData.weight}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                user.weight
+              )}
+              </td>
+              <td>{editingId === user.id ? (
+                <input
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                user.phone
+              )}
+              </td>
               <td>
                 {console.log(user)}
-
-                <Link
-                  to={{
-                    pathname:
-                      user.type == "donor"
-                        ? "/nurse/donorEdit"
-                        : "/nurse/recipientEdit",
-                    state: { user: user },
-                  }}
-                >
-                  <button className="action-btn edit-btn">Edit</button>
-                </Link>
+                {editingId === user.id ? (
+                  <button
+                    className="action-btn save-btn"
+                    onClick={() => saveChanges(user)}
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <button
+                    className="action-btn edit-btn"
+                    onClick={() => startEditing(user)}
+                  >
+                    Edit
+                  </button>
+                )}
                 <button
                   className="action-btn delete-btn"
                   onClick={() => deleteUser(user.id, user.type)}
