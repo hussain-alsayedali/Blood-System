057bde4cd39839785ae5e1eed1673252c1868f8b
Hussain <hussain.alsayedali@gmail.com>
2024-01-15 04:59:49 +0300

    frontend selected

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const nodemailer = require("nodemailer");
 
 // cors
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://bloodwave.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true"); // If you need to allow credentials (e.g., cookies)
