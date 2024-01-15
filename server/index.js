1e527c7707832dbe45546742b6afe436337c7cf7
Hussain <hussain.alsayedali@gmail.com>
2024-01-15 04:54:04 +0300

    cors issue hopefully fixed

--- server/index.js
+++ server/index.js
@@ -26,6 +26,17 @@ const { data } = require("autoprefixer");
 const nodemailer = require("nodemailer");
 
 //const authController = require("./controllers/auth");
+
+// cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // If you need to allow credentials (e.g., cookies)
+
+  next();
+});
+
 //enablieng cors
 app.use(
   cors({
